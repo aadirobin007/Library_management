a3cbb2635136cc38cd996b600edaa3e64ea116b1
aadirobin007 <168745222+aadirobin007@users.noreply.github.com>
2026-02-26 20:01:06 +0530

    onfigure MySQL connection for Railway

--- db/config.php
+++ db/config.php
@@ -1,13 +1,13 @@
 <?php
-$host = "switchyard.proxy.rlwy.net";
-$port = 11071;
-$user = "root";
-$pass = "ArbOFnCtxHMGgjPjumlwxoPJplCazMJg";
-$db   = "railway";
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
+$db   = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("DB Connection failed: " . $conn->connect_error);
 }
 ?>
