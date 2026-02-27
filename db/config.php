0de24dffec07b8d13f357d83a25dd1eabeae73dc
aadirobin007 <168745222+aadirobin007@users.noreply.github.com>
2026-02-27 13:35:21 +0530

    Commit and push

--- db/config.php
+++ db/config.php
@@ -1,11 +1,11 @@
 <?php
-$conn = new mysqli(
-    getenv("DB_HOST"),
-    getenv("DB_USER"),
-    getenv("DB_PASSWORD"),
-    getenv("DB_NAME"),
-    getenv("DB_PORT")
-);
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
